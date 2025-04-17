bc9de334e08ee7e3b363873b8a1db39be450e3f0
RohitBCA456 <ry2281104@gmail.com>
2025-04-17 13:41:37 +0530

    fixed errors and bugs

--- cart/app.js
+++ cart/app.js
@@ -1,13 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cartRouter from "./router/cart.router.js";
 import { connect } from "./service/RabbitMQ.js";
 import cookieParser from "cookie-parser";
-dotenv.config({path: "./.env"});
+dotenv.config({ path: "./.env" });
 const app = express();
 connect();
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE",
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended: true}));
-app.use("/",cartRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use("/", cartRouter);
 export { app };
